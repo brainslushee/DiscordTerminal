fd1191fb6f102aefa5f4cb98c4d1fa385528b398
Austin Smith <austindsmith15@gmail.com>
2018-05-11 14:13:24 -0600

    comments

--- src/connect.py
+++ src/connect.py
@@ -5,6 +5,8 @@
 
 import discord
 
+
+#Gets username and password, move this inside of a function and call from a main python file
 username = input("Enter username: ")
 password = input("Enter password: ")
 
