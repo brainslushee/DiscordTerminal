d4d4fdc05cbc5d48dfbb28f148555b6a50da7a3a
Austin Smith <austindsmith15@gmail.com>
2018-05-16 23:04:36 -0600

    Testing logging in...

--- src/client.py
+++ src/client.py
@@ -4,3 +4,12 @@ import discord
 import asyncio
 
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.close()
