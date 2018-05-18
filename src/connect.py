3097127a5199fc11a09e8280d91c5ef7888c15e5
Kyle <kmayer5120@hotmail.com>
2018-05-18 17:06:40 -0600

    found and implemented a solution for password being displayed

--- src/connect.py
+++ src/connect.py
@@ -2,12 +2,14 @@
 
 import discord
 import asyncio
+import getpass
 
 
 #Gets username and password, move this inside of a function and call from a main python file
 def getUserInfo():
     username = input("Enter username: ")
-    password = input("Enter password: ")
+    #getpass works like Linux password entry. (Does not display chars for password)
+    password = getpass.getpass("Enter password: ")
     #Currently a bot created through Discord API is required for this token
     #token = input("Enter token: ")
 
