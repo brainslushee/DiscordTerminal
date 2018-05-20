53b8e9c3d6029164ffef4dd5d2332e1c26d6ea60
kmayer5120 <kmayer5120@hotmail.com>
2018-05-20 17:33:01 -0600

    added splash screen for style points

--- src/main.py
+++ src/main.py
@@ -17,6 +17,24 @@ from ConfigManager import ConfigManager
 #Add functions from connect as needed
 from connect import getUserInfo, getToken#, printServers, closeClient
 
+splashScreen = '''
+----------------------------------------------------
+  ____ ___ ____   ____ ___  ____  ____  
+  |  _ \_ _/ ___| / ___/ _ \|  _ \|  _ \ 
+  | | | | |\___ \| |  | | | | |_) | | | |
+  | |_| | | ___) | |__| |_| |  _ <| |_| |
+  |____/___|____/ \____\___/|_| \_\____/ 
+    _____ _____ ____  __  __ ___ _   _    _    _     
+  |_   _| ____|  _ \|  \/  |_ _| \ | |  / \  | |    
+    | | |  _| | |_) | |\/| || ||  \| | / _ \ | |    
+    | | | |___|  _ <| |  | || || |\  |/ ___ \| |___ 
+    |_| |_____|_| \_\_|  |_|___|_| \_/_/   \_\_____|
+----------------------------------------------------
+    '''
+
+print(splashScreen)
+
+
 #this is a temporary menu that could be extracted into a function
 print("Login Menu: ")
 print("\t1. use config file")
