d5af7f499048df0ed815bfcd30b6f5bb67cdd9e1
Austin <austin@slushsites.com>
2018-05-22 18:49:27 -0600

    Print server now in a function

--- src/connect.py
+++ src/connect.py
@@ -7,22 +7,21 @@ import getpass
 #Gets username and password
 def getUserInfo():
     username = input("Enter email address: ")
-    
+
     #getpass works like Linux password entry. (Does not display chars for password)
     password = getpass.getpass("Enter password: ")
     print("One moment please, we are trying to connect...")
-    
+
     info = {
         'username': username,
         'password': password,
         }
     return info
-
-#Doesn't work yet... don't know why.
+#Prints servers
 async def printServers(client):
     print("Available servers: ")
     for server in client.servers:
         print(server.name)
-        
+
 async def closeClient(client):
     await client.close()
